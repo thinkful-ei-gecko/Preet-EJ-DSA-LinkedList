89ba9a074f5bd350707b203bbdb988c579a6b582
EJ <ejg132@gmail.com>
2019-11-07 12:40:01 -0800

    finished #2

--- linked-list.js
+++ linked-list.js
@@ -55,11 +55,11 @@ class LinkedList {
     let currNode = this.head;
     let previousNode = this.head;
 
-    while ((currNode !== null) && (currNode.value !== item)) {
+    while ((currNode !== undefined) && (currNode.value !== item)) {
         previousNode = currNode;
         currNode = currNode.next;
     }
-    if (currNode === null) {
+    if (currNode === undefined) {
         console.log('Item not found');
         return;
     }
@@ -72,7 +72,7 @@ class LinkedList {
     let currTarget = 0;
     let newNode = new _Node(item)
     
-    if(currTarget === 0){
+    if(target === 0){
       newNode.next = currNode;
       this.head = newNode
     }
@@ -84,26 +84,25 @@ class LinkedList {
     }
     newNode.next = currNode;
     previousNode.next = newNode;
+
   }
 
   insertAfter(item, target){
     let currNode = this.head;
-    let previousNode = this.head;
     let currTarget = 0;
     let newNode = new _Node(item)
     
-    if(currTarget > 0){
-      newNode.next = currNode;
-      this.head = newNode
-    }
+    // if(currTarget > 0){
+    //   newNode.next = currNode;
+    //   this.head = newNode
+    // }
     
-    else while(currTarget > target){
+    while(currTarget < target){
       currTarget++;
-      previousNode = currNode;
       currNode = currNode.next;
     }
-    newNode.next = currNode;
-    previousNode.next = newNode;
+    newNode.next = currNode.next;
+    currNode.next = newNode;
   }
 
   insertAt(item, target){
@@ -112,24 +111,24 @@ class LinkedList {
     let currTarget = 0;
     let newNode = new _Node(item)
 
-    for(let i = 0; i < target - 1; i++){
-      previousNode = currNode;
-      currNode = currNode.next;
-      if(currTarget === 0){
-        newNode.next = currNode;
-        this.head = newNode;
+      while (currTarget < target){
+        currTarget++
+        previousNode = currNode;
+        currNode = currNode.next;
       }
-    }
+
+      previousNode.next = newNode;
+      newNode.next = currNode;
+
   }
 
   insert(item){
     let currNode = this.head;
-    let newNode = new _Node(item, currNode.next);
+    let newNode = new _Node(item);
     if(this.head === null){
       this.head = newNode;
     }
     else{
-      currNode = this.head;
       while(currNode.next){
         currNode = currNode.next;
       }
@@ -141,17 +140,17 @@ class LinkedList {
 function main(){
     let SLL = new LinkedList();
     SLL.insertFirst('Apollo');
-    SLL.insertLast('Boomer');
-    SLL.insertLast('Helo');
-    // SLL.insert('Husker');
-    // SLL.insert('Starbuck');
-    // SLL.insert('Tauhida');
-    // SLL.remove('squirrel');
-    // SLL.insertBefore('Athena', 2);
-    // SLL.insertAfter('Hotdog', 3);
-    // SLL.insertAt('Kat', 3);
-    // SLL.remove('Tauhida');
+    SLL.insert('Boomer');
+    SLL.insert('Helo');
+    SLL.insert('Husker');
+    SLL.insert('Starbuck');
+    SLL.insert('Tauhida');
+    SLL.remove('squirrel');
+    SLL.insertBefore('Athena', 1);
+    SLL.insertAfter('Hotdog', 3);
+    SLL.insertAt('Kat', 2);
+    SLL.remove('Tauhida');
     return SLL;
   }
 
-console.log(main());
+console.log(JSON.stringify(main()));
